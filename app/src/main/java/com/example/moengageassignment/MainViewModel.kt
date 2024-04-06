840a17ccd8ae786f5193e10793c07c30d2792334
prashant upadhyay <prashant.upadhyay@kutumbapp.com>
2024-04-06 20:47:10 +0530

    migrated to state flow

--- app/src/main/java/com/example/moengageassignment/MainViewModel.kt
+++ app/src/main/java/com/example/moengageassignment/MainViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.moengageassignment
 
 import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -13,6 +15,10 @@ import com.example.moengageassignment.domain.GetNewsArticlesUseCase
 import com.example.moengageassignment.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -21,8 +27,9 @@ class MainViewModel @Inject constructor(
     private val getNewsArticlesUseCase: GetNewsArticlesUseCase
 ) : ViewModel() {
     var showSplash by mutableStateOf(true)
-    private val _newsArticles = MutableLiveData<Resource<List<NewsArticle>>>()
-    val newsArticles: LiveData<Resource<List<NewsArticle>>> get() = _newsArticles
+    private val _newsArticles: MutableStateFlow<Resource<List<NewsArticle>>> =
+        MutableStateFlow(Resource.Loading())
+    val newsArticle: StateFlow<Resource<List<NewsArticle>>> = _newsArticles.asStateFlow()
 
     init {
         fetchNewsArticles()
@@ -31,15 +38,15 @@ class MainViewModel @Inject constructor(
 
     private fun fetchNewsArticles() {
         viewModelScope.launch(Dispatchers.IO) {
-            _newsArticles.postValue(Resource.Loading())
+            _newsArticles.emit(Resource.Loading())
             when (val res = getNewsArticlesUseCase()) {
                 is Resource.Success -> {
-                    _newsArticles.postValue(Resource.Success(res.data!!))
+                    _newsArticles.value = Resource.Success(res.data!!)
                     Log.d("NewsResponseTest", res.data.toString())
                 }
 
                 is Resource.DataError -> {
-                    _newsArticles.postValue(Resource.DataError(res.error!!))
+                    _newsArticles.value = Resource.DataError(res.error!!)
                     Log.d("NewsResponseTest", res.error.toString())
                 }
 
--- app/src/main/java/com/example/moengageassignment/ui/module/home/NewsHome.kt
+++ app/src/main/java/com/example/moengageassignment/ui/module/home/NewsHome.kt
@@ -1,22 +1,55 @@
 package com.example.moengageassignment.ui.module.home
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.moengageassignment.MainViewModel
+import com.example.moengageassignment.utils.Resource
 
 @Composable
 fun NewsHome(mainViewModel: MainViewModel) {
     Scaffold {
+        val newsArticleState by mainViewModel.newsArticle.collectAsState()
+        when (newsArticleState) {
+            is Resource.Success -> {
+
+            }
+
+            is Resource.Loading -> LoadingWidget()
+
+            is Resource.DataError -> {}
+        }
         Column(modifier = Modifier.padding(it)) {
 
         }
     }
+}
+
+@Composable
+fun LoadingWidget() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun NewsWidget() {
+
 }
 
 
