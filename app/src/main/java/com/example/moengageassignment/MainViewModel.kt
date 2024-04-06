a79170283829b58d145b3581a2b26a465e7d945b
prashant upadhyay <prashant.upadhyay@kutumbapp.com>
2024-04-06 18:13:53 +0530

    bug fix

--- app/src/main/java/com/example/moengageassignment/MainViewModel.kt
+++ app/src/main/java/com/example/moengageassignment/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.moengageassignment
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -11,6 +12,7 @@ import com.example.moengageassignment.data.dto.NewsArticle
 import com.example.moengageassignment.domain.GetNewsArticlesUseCase
 import com.example.moengageassignment.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -28,18 +30,21 @@ class MainViewModel @Inject constructor(
 
 
     private fun fetchNewsArticles() {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             _newsArticles.postValue(Resource.Loading())
             when (val res = getNewsArticlesUseCase()) {
                 is Resource.Success -> {
                     _newsArticles.postValue(Resource.Success(res.data!!))
+                    Log.d("NewsResponseTest", res.data.toString())
                 }
 
                 is Resource.DataError -> {
                     _newsArticles.postValue(Resource.DataError(res.error!!))
+                    Log.d("NewsResponseTest", res.error.toString())
                 }
 
                 else -> {}
+
             }
         }
     }
