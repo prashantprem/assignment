941e644b8e7235f007403a900a1842c7cf2c0cd1
prashant upadhyay <prashant.upadhyay@kutumbapp.com>
2024-04-07 13:07:55 +0530

    package id updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.moengageassignment"
+        applicationId = "mango.news.moengageassignment"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
